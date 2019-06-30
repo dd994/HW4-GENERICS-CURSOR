a45a424d5e4456ea0de499c8a7fc5ea1636e647d
Dmytro Dzhunenko <ddzhunenko@gmail.com>
2019-06-30 19:29:16 +0300

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,7 @@ public class Main {
         banknoteCounter(number);
 
         //upd
+
         banknoteRuntimeCounter();
     }
 }
--- src/main/java/counters/BanknoteCounter.java
+++ src/main/java/counters/BanknoteCounter.java
@@ -19,20 +19,20 @@ public class BanknoteCounter {
         banknotes.add(2);
         banknotes.add(1);
 
-        Iterator<Integer> iterator = banknotes.iterator(); //визначаємо ітератор
-        Integer number = iterator.next(); //присвоємо значення номіналу -  поточному елементу списку
-        int currNum = amount; //об"явимо змінну, яка спочатку дорівнюватиме введеній сумі
+        Iterator<Integer> iterator = banknotes.iterator(); //define an iterator
+        Integer number = iterator.next(); //assign the value of the nominal value to the current item in the list
+        int currNum = amount; //declare a variable that initially equals the amount entered
 
-        while (iterator.hasNext()) { //поки є наступний елемент :
-            int meter = currNum / number; //об"являємо лічильник, результатом буде кількість націло поділених купюр
-            if (currNum >= number) { //якщо поточне число дорівнює чи більша за номінал
-                System.out.println(number + " - " + meter); //друкуємо результат
-                currNum %= number;//далі в цьому ж  циклі отримуємо залишок від ділення на ціло і присвоємо цей залишок поточному числу
+        while (iterator.hasNext()) { //while there is the following element:
+            int meter = currNum / number; //declare a counter, the result will be the amount of entirely divided banknotes
+            if (currNum >= number) { //if the current number is equal to or greater than the nominal value
+                System.out.println(number + " - " + meter); //print the result
+                currNum %= number;// in the same cycle we get the remainder from the division by integer and assign this remainder to the current number
             }
-            if (currNum < number) { //якщо поточно число менше поточного номіналу - змінюємо номінал
+            if (currNum < number) { // if the current number is less than the current denomination - change the denomination
                 number = iterator.next();
             }
-            if (currNum == 0) { //якщо поточне число дорівнює нулю - нема що зрівнювати з номіналом - виход з циклу
+            if (currNum == 0) {
                 break;
             }
         }
